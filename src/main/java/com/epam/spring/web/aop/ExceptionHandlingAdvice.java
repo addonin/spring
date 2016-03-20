e97db02179ce3cea79de240fd38b8807258a7a78
dimon <dmitrii.adonin@gmail.com>
2016-03-20 23:40:40 +0200

    Exception handling

---
+++ src/main/java/com/epam/spring/web/aop/ExceptionHandlingAdvice.java
@@ -0,0 +1,22 @@
+package com.epam.spring.web.aop;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author dimon
+ * @since 20/03/16.
+ */
+@ControllerAdvice
+public class ExceptionHandlingAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", e.getMessage());
+        modelAndView.setViewName("exception");
+        return modelAndView;
+    }
+
+}
